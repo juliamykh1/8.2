56a459bcc58b1c778064c57f926c6a5aea2a78e2
Julia <mihajloviculia65@gmail.com>
2023-03-27 18:02:24 +0300

    first master commit

---
+++ 82/82.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+void foo(std::string& input)
+{
+    // Викидаєм все після першого пробілу
+    const std::string beforeSpaceInput = input.substr(0, input.find(32));
+
+    input = beforeSpaceInput;
+
+    // викидаєм все що не є буквою або цифрою
+    input.erase(std::remove_if(input.begin(), input.end(), [](char c) { return !std::isalpha(c); }), input.end());
+
+    // Робимо символ малим, якщо це можливо
+    std::for_each(input.begin(), input.end(), [](char& ch) {
+        ch = std::tolower(static_cast<unsigned char>(ch));
+        });
+}
+
+int main()
+{
+    std::string input;
+
+    std::cout << "Enter some characters\n> ";
+    std::getline(std::cin, input);
+
+    foo(input);
+
+    std::cout << "Transformed input\n> " << input << std::endl;
+}
---
+++ test8.2/test8.2.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include "../82/82.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test82
+{
+	TEST_CLASS(test82)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			std::string input = "SoMe-bAsIc-STRING with spaces";
+			std::string result = "somebasicstring";
+
+			foo(input);
+
+			std::cout << input << std::endl;
+
+			Assert::AreEqual(result, input);
+		}
+	};
+}
